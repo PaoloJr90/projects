f4079872b4411553ab7cf407362441548e248edc
PJ Angeloni <pj.angeloni@gmail.com>
2023-07-05 15:20:57 -0400

    changes to slider and formatting

--- mortgage-calculator-react-app/src/Components/Common/SliderComponent.js
+++ mortgage-calculator-react-app/src/Components/Common/SliderComponent.js
@@ -1,8 +1,35 @@
 import React from "react";
-import Slider from '@mui/material/Slider';
+import Slider from "@mui/material/Slider";
+import { Stack, Typography } from "@mui/material";
 
-const SliderComponent = () => {
-  return <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
+const SliderComponent = ({ defaultValue, min, max, step, onChange, value }) => {
+  return (
+    <>
+      <Stack gap={1} direction="column">
+        <Typography variant="subtitle2">Property Value</Typography>
+        <Typography variant="h5">$ 3000</Typography>
+      </Stack>
+      <Slider
+        defaultValue={defaultValue}
+        min={min}
+        max={max}
+        aria-label="Default"
+        valueLabelDisplay="auto"
+        marks={true}
+        step={step}
+        onChange={onChange}
+        value={value}
+      />
+      <Stack direction="row" justifyContent={"space-between"}>
+        <Typography variant="caption" color="text.secondary">
+          $1000
+        </Typography>
+        <Typography variant="caption" color="text.secondary">
+          $10000
+        </Typography>
+      </Stack>
+    </>
+  );
 };
 
 export default SliderComponent;
--- mortgage-calculator-react-app/src/Components/SliderSelect.js
+++ mortgage-calculator-react-app/src/Components/SliderSelect.js
@@ -2,7 +2,17 @@ import React from "react";
 import SliderComponent from "./Common/SliderComponent";
 
 const SliderSelect = () => {
-  return <SliderComponent />;
+  return (
+    <>
+      <SliderComponent
+        min={0}
+        max={50}
+        defaultValue={20}
+        step={5}
+        onChange={(event) => console.log(event.target.value)}
+      />
+    </>
+  );
 };
 
 export default SliderSelect;
